57414c1c853407e882bbe1ef8977783c68ef12e8
kidfrom <jason.onggo@tempatkerja.com>
2020-08-12 20:48:08 +0700

    update the localStorage.js, now use Objects inside Array

--- learn_offline-storage/assets/js/localStorage.js
+++ learn_offline-storage/assets/js/localStorage.js
@@ -14,12 +14,14 @@ function submit() {
 function push(inputFirstName, inputIDNumber, inputIDPIC, inputIntention) {
     let lists = JSON.parse(localStorage.lists);
 
+    // logic: idNumbers is unique.
     let idNumbers = [];
     for (let i = 0; i < lists.length; i++) {
         idNumbers.push(lists[i].idNumber);
     }
 
     if (!idNumbers.includes(inputIDNumber)) {
+        // push the objects to the lists.
         let tempObjects = {};
         tempObjects.idNumber = inputIDNumber;
         tempObjects.firstName = inputFirstName;
@@ -38,7 +40,27 @@ function view() {
     document.getElementById("viewList").innerHTML = '';
 
     for (let i = 0; i < JSON.parse(localStorage.lists).length; i++) {
-        console.log(JSON.parse(localStorage.lists)[i].firstName);
-        document.getElementById("viewList").innerHTML += `<li class="list-group-item">${JSON.parse(localStorage.lists)[i].firstName}</li>`
+        // convert idPIC to namePIC
+        let namePIC;
+        switch(JSON.parse(localStorage.lists)[i].idPIC) {
+            case "1":
+                namePIC = "Jack Ma";
+                break;
+            case "2":
+                namePIC = "Jeff Bezos";
+                break;
+            case "3":
+                namePIC = "Elon Musk";
+                break;
+        }
+
+        // innerHTML
+        document.getElementById("viewList").innerHTML += `  
+            <th scope="row">${i+1}</th>
+                <td>${JSON.parse(localStorage.lists)[i].idNumber}</td>
+                <td>${JSON.parse(localStorage.lists)[i].firstName}</td>
+                <td>${namePIC}</td>
+            <td>${JSON.parse(localStorage.lists)[i].intention}</td>
+        `;
     }
 }
