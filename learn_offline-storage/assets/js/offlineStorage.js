066bcd209c6b3b69529c81c8ee6451def3d51967
kidfrom <jason.onggo@tempatkerja.com>
2020-08-12 15:02:05 +0700

    add national id number, TODO: viewable only by admin

--- learn_offline-storage/assets/js/offlineStorage.js
+++ learn_offline-storage/assets/js/offlineStorage.js
@@ -1,15 +1,19 @@
+let idNumber = [];
+
 function submit() {
-    let firstName = document.getElementById('inputFirstName').value;
-    push(firstName);
+    let inputFirstName = document.getElementById('inputFirstName').value;
+    let inputIDNumber = document.getElementById('inputIDNumber').value;
+    push(inputFirstName, inputIDNumber);
 }
 
-function push(firstName) {
+function push(inputFirstName, inputIDNumber) {
     let lists = document.getElementsByClassName("list-group-item");
     let data = [];
     for (let i = 0; i < lists.length; i++) {
         data.push(lists[i].innerHTML);
     }
-    if (!data.includes(firstName)) {
-        document.getElementById("viewList").innerHTML += `<li class="list-group-item">${firstName}</li>`
+    if (!data.includes(inputFirstName) && !idNumber.includes(inputIDNumber)) {
+        idNumber.push(inputIDNumber);
+        document.getElementById("viewList").innerHTML += `<li class="list-group-item">${inputFirstName}</li>`
     }
 }
