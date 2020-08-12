71c90f63f08394f004ec6d8fb11ebd524e098e49
kidfrom <jason.onggo@tempatkerja.com>
2020-08-12 15:38:19 +0700

    add local storage support

--- learn_offline-storage/assets/js/offlineStorage.js
+++ learn_offline-storage/assets/js/offlineStorage.js
@@ -1,4 +1,5 @@
-let idNumber = [];
+let names = JSON.parse(localStorage.names);
+let idNumbers = JSON.parse(localStorage.idNumbers);
 
 function submit() {
     let inputFirstName = document.getElementById('inputFirstName').value;
@@ -7,13 +8,33 @@ function submit() {
 }
 
 function push(inputFirstName, inputIDNumber) {
-    let lists = document.getElementsByClassName("list-group-item");
-    let data = [];
-    for (let i = 0; i < lists.length; i++) {
-        data.push(lists[i].innerHTML);
+    // let lists = document.getElementsByClassName("list-group-item");
+    // let data = [];
+    // for (let i = 0; i < lists.length; i++) {
+    //     data.push(lists[i].innerHTML);
+    // }
+    if (!names.includes(inputFirstName) && !idNumbers.includes(inputIDNumber)) {
+        names.push(inputFirstName);
+        idNumbers.push(inputIDNumber);
+
+        // store to local storage
+        if (localStorage.names == "" || localStorage.idNumbers == "") {
+            localStorage.names = "[]";
+            localStorage.idNumbers = "[]";
+        } else {
+            localStorage.names = JSON.stringify(names);
+            localStorage.idNumbers = JSON.stringify(idNumbers);
+        }
+        // document.getElementById("viewList").innerHTML += `<li class="list-group-item">${inputFirstName}</li>`
     }
-    if (!data.includes(inputFirstName) && !idNumber.includes(inputIDNumber)) {
-        idNumber.push(inputIDNumber);
-        document.getElementById("viewList").innerHTML += `<li class="list-group-item">${inputFirstName}</li>`
+}
+
+function view() {
+    // reset
+    document.getElementById("viewList").innerHTML = '';
+
+    for (let i = 0; i < JSON.parse(localStorage.names).length; i++) {
+        console.log(i);
+        document.getElementById("viewList").innerHTML += `<li class="list-group-item">${JSON.parse(localStorage.names)[i]}</li>`
     }
 }
