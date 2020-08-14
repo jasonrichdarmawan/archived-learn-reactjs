c81d89f00b24f1fa4615a6081918bd627c7e2e94
kidfrom <jason.onggo@tempatkerja.com>
2020-08-14 15:16:06 +0700

    add array filter and map example

---
+++ learn_array/assets/js/array.js
@@ -0,0 +1,46 @@
+function filter(request) {
+    let tempArray = [10, 23, 456, 21, 77, 85, 19, 92, 43, 35];
+
+    let result = tempArray.filter((number) => {
+        // prime number
+        if (request === "prime number") {
+            for (let i = 2; i < number; i++) {
+                if (number % i === 0) return false;
+            }
+            return true;
+        }
+        // divisible by 3
+        else if (request === "divisible by 3") {
+            if (number % 3 === 0) return true;
+        }
+        // result more than 636 after multiplied by 17
+        else if (request === "more than 636 after multiplied by 17") {
+            if (number * 17 > 636) return true;
+        }
+    });
+
+    console.log(result);
+}
+
+function map(request) {
+    let tempArray = [10, 23, 456, 21, 77, 85, 19, 92, 43, 35];
+    let result = tempArray.map((number) => {
+        // value of circle area
+        if (request === "circle area") {
+            // pi * r^2
+            return 22 / 7 * Math.pow(number, 2);
+        }
+        // value of square area
+        else if (request === "square area") {
+            // s^2
+            return Math.pow(number, 2);
+        }
+        // value of cubic volume
+        else if (request === "cubic volume") {
+            // s^3
+            return Math.pow(number, 3);
+        }
+    });
+
+    console.log(result);
+}
