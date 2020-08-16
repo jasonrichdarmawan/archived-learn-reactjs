831f0486d1b9ef11f9682182b82d7d2d85ce8d6f
kidfrom <jason.onggo@tempatkerja.com>
2020-08-16 18:38:40 +0700

    add temporary logic to generate and store Authorization token

--- learn_reactjs/src/App.js
+++ learn_reactjs/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
 import LoginPage from './components/LoginPage/LoginPage';
 
 function App() {
-  return (
-    <LoginPage />
-  );
+  // TODO: research safe way to store Authorization token.
+  if (localStorage.Authorization === undefined) {
+    return (
+      <LoginPage />
+    );
+  } else {
+    return (
+      <div>HomePage</div>
+    )
+  }
 }
 
 export default App;
--- learn_reactjs/src/components/LoginPage/LoginForm.js
+++ learn_reactjs/src/components/LoginPage/LoginForm.js
@@ -2,26 +2,31 @@ import React from 'react';
 import logo from '../../logo.svg';
 
 class LoginForm extends React.Component {
-    render() {
-        return (
-            <div className="min-vh-100 d-flex align-items-center">
-            <div className="container w-25 text-center">
-              <form>
-                <img className="mb-3" src={logo} alt=""/>
-                <div className="form-group">
-                  <input type="email" className="form-control" id="inputEmail" placeholder="Email address"></input>
-                  <input type="password" className="form-control" id="inputPassword" placeholder="Password"></input>
-                </div>
-                <div className="form-check mb-3">
-                  <input type="checkbox" className="form-check-input" id="inputRemember"></input>
-                  <label className="form-check-label" htmlFor="inputRemember">Remember</label>
-                </div>
-                <button type="submit" className="btn btn-primary btn-block">Sign In</button>
-              </form>
-            </div>
-          </div>
-        );
+  render() {
+    // TODO: research safe way to generate Authorization token.
+    function generateToken() {
+      localStorage.Authorization = "Bearer " + Math.random().toString(36).substr(2);
     }
+    
+    return (
+      <div className="min-vh-100 d-flex align-items-center">
+        <div className="container w-25 text-center">
+          <form>
+            <img className="mb-3" src={logo} alt=""/>
+            <div className="form-group">
+              <input type="email" className="form-control" id="inputEmail" placeholder="Email address"></input>
+              <input type="password" className="form-control" id="inputPassword" placeholder="Password"></input>
+            </div>
+            <div className="form-check mb-3">
+              <input type="checkbox" className="form-check-input" id="inputRemember"></input>
+              <label className="form-check-label" htmlFor="inputRemember">Remember</label>
+            </div>
+            <button type="submit" className="btn btn-primary btn-block" onClick={generateToken}>Sign In</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default LoginForm;
