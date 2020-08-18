e8d804059d124ea13e6cdb583e941a823ab69a9c
kidfrom <jason.onggo@tempatkerja.com>
2020-08-18 16:56:10 +0700

    move dummyData to App.js

--- learn_reactjs/src/App.js
+++ learn_reactjs/src/App.js
@@ -13,7 +13,153 @@ import ListsPage from './components/ListsPage/ListsPage';
 import CreatePage from './components/CreatePage/CreatePage';
 import ViewPage from './components/ViewPage/ViewPage';
 
+function dummyData() {
+  // debug only: dummy data
+  let employee_primary = [
+      {},
+      {},
+      {
+          id: "2",
+          name: "Jeff Bezos Long Name",
+          email: "jeff_bezos_long_name@gmail.com"
+      },
+      {},
+      {
+          id: "4",
+          name: "Jeff Bezos Long Name",
+          email: "jeff_bezos_long_name@gmail.com"
+      },
+      {},
+      {
+          id: "6",
+          name: "Jeff Bezos Long Name",
+          email: "jeff_bezos_long_name@gmail.com"
+      },
+      {},
+      {
+          id: "8",
+          name: "Jeff Bezos Long Name",
+          email: "jeff_bezos_long_name@gmail.com"
+      },
+  ];
+  localStorage.employee_primary = JSON.stringify(employee_primary);
+  let employee_info = [
+      {},
+      {},
+      {
+          primary_id: "2",
+          nationalIDNumber: "2222",
+          idValidityDate: "2020-20-20",
+          taxpayerIDNumber: "2222",
+          taxPeriod: "2020-20-20",
+          birthDate: "2020-20-20",
+          nationality: "ID",
+          gender: "Male",
+          religion: "Islam",
+          maritalStatus: "Not married",
+          education: "Postgraduate"
+      },
+      {},
+      {
+          primary_id: "4",
+          nationalIDNumber: "5555",
+          idValidityDate: "2020-20-20",
+          taxpayerIDNumber: "5555",
+          taxPeriod: "2020-20-20",
+          birthDate: "2020-20-20",
+          nationality: "ID",
+          gender: "Male",
+          religion: "Islam",
+          maritalStatus: "Not married",
+          education: "Postgraduate"
+      },
+      {},
+      {
+          primary_id: "6",
+          nationalIDNumber: "6666",
+          idValidityDate: "2020-20-20",
+          taxpayerIDNumber: "6666",
+          taxPeriod: "2020-20-20",
+          birthDate: "2020-20-20",
+          nationality: "ID",
+          gender: "Male",
+          religion: "Islam",
+          maritalStatus: "Not married",
+          education: "Postgraduate"
+      },
+      {},
+      {
+          primary_id: "8",
+          nationalIDNumber: "8888",
+          idValidityDate: "2020-20-20",
+          taxpayerIDNumber: "8888",
+          taxPeriod: "2020-20-20",
+          birthDate: "2020-20-20",
+          nationality: "ID",
+          gender: "Male",
+          religion: "Islam",
+          maritalStatus: "Not married",
+          education: "Postgraduate"
+      },
+  ];
+  localStorage.employee_info = JSON.stringify(employee_info);
+  let department_primary = [
+      {},
+      {},
+      {
+          id: "2",
+          name: "Board of Directors",
+          email: "bod@gmail.com"
+      },
+      {},
+      {
+          id: "4",
+          name: "C-Level Executives",
+          email: "c-level@gmail.com"
+      },
+      {},
+      {
+          id: "6",
+          name: "Operation",
+          email: "operation@gmail.com"
+      },
+      {},
+      {
+          id: "8",
+          name: "Marketing",
+          email: "marketing@gmail.com"
+      },
+  ];
+  localStorage.department_primary = JSON.stringify(department_primary);
+  let department_info = [
+      {},
+      {},
+      {
+          primary_id: "2",
+          employeeList: "[2]"
+      },
+      {},
+      {
+          primary_id: "4",
+          employeeList: "[4]"
+      },
+      {},
+      {
+          primary_id: "6",
+          employeeList: "[6]"
+      },
+      {},
+      {
+          primary_id: "8",
+          employeeList: "[8]"
+      }
+  ];
+  localStorage.department_info = JSON.stringify(department_info);
+}
+
 function App() {
+  dummyData();
+  
   // TODO: research safe way to store Authorization token.
   if (localStorage.Authorization === undefined) {
     return (
