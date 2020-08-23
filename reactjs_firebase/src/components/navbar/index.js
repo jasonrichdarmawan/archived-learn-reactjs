ffe311fd89b150a42f4af51b97cf0c89c2ad4d0f
kidfrom <jason.onggo@tempatkerja.com>
2020-08-23 19:44:53 +0700

    add logout function

--- reactjs_firebase/src/components/navbar/index.js
+++ reactjs_firebase/src/components/navbar/index.js
@@ -1,10 +1,14 @@
 import React, { useContext } from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Button } from "react-bootstrap";
 import { AuthDataContext } from "../../providers/authdata";
 import { Link } from "react-router-dom";
+import firebase from "../../providers/firebase";
 
 export const NavBar = () => {
   const user = useContext(AuthDataContext);
+  const logOut = () => {
+    firebase.auth().signOut()
+  }
   if (user) {
     return (
       <Navbar bg="light" expand="lg">
@@ -14,6 +18,7 @@ export const NavBar = () => {
           <Nav className="mr-auto">
             <Link to="/list" className="nav-link">List</Link>
           </Nav>
+          <Button variant="danger" size="sm" onClick={logOut}>Log Out</Button>
         </Navbar.Collapse>
       </Navbar>
     );
