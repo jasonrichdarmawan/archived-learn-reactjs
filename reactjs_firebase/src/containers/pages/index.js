878084437dd451b37b6d245305d0d31fe93cba52
kidfrom <jason.onggo@tempatkerja.com>
2020-08-24 15:28:40 +0700

    first commit

--- reactjs_firebase/src/containers/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { LogIn } from "./login";
-export { Register } from "./register"
-export { PasswordReset } from "./passwordreset";
-export { Dashboard } from "./dashboard";
-export { List } from "./list";
