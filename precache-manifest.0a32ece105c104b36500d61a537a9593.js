b2d57ae813238c6ccd42f0059d89572134c3fe2e
kidfrom <jason.onggo@tempatkerja.com>
2020-08-28 10:29:35 +0700

    Updates

---
+++ precache-manifest.0a32ece105c104b36500d61a537a9593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04c97a0a708e30b1804b788f4da7c6b8",
+    "url": "/bc_g2_learn_reactjs/index.html"
+  },
+  {
+    "revision": "d75ef28085c33845ca9c",
+    "url": "/bc_g2_learn_reactjs/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d75ef28085c33845ca9c",
+    "url": "/bc_g2_learn_reactjs/static/js/2.a570471e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bc_g2_learn_reactjs/static/js/2.a570471e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c8beea22e09d7168e7e",
+    "url": "/bc_g2_learn_reactjs/static/js/main.dd70d8ff.chunk.js"
+  },
+  {
+    "revision": "9ccfe45963c17230b0c0",
+    "url": "/bc_g2_learn_reactjs/static/js/runtime-main.35ee7ad8.js"
+  }
+]);
