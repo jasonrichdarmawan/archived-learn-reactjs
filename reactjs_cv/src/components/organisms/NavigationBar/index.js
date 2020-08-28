98db782ce00ac00e98245d6a65237bdc5ed5a34c
kidfrom <jason.onggo@tempatkerja.com>
2020-08-28 11:05:48 +0700

    fix navbar collapse for mobile view

--- reactjs_cv/src/components/organisms/NavigationBar/index.js
+++ reactjs_cv/src/components/organisms/NavigationBar/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 
 export const NavigationBar = (props) => (
-  <Navbar bg="light" expand="lg">
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        {props.routesJSX}
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+<Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">Jason's CV</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+    {props.routesJSX}
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
 );
