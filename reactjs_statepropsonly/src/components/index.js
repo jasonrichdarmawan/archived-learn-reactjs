325dff826c5143b3cea0cb0eaeb9ccba7efba5f9
kidfrom <jason.onggo@tempatkerja.com>
2020-08-31 15:38:26 +0700

    remove export atoms, molecules, and organisms

--- reactjs_statepropsonly/src/components/index.js
+++ reactjs_statepropsonly/src/components/index.js
@@ -1,5 +1,5 @@
 // Notice: ./atoms - ./organisms is an attempt to re-create how React Bootstrap works. The actual project will use React bootstrap to speed up the development.
 
-export * from "./atoms";
-export * from "./molecules";
-export * from "./organisms";
+// export * from "./atoms";
+// export * from "./molecules";
+// export * from "./organisms";
