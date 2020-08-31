7aa30a95430f2cc4595c0b6e0fa3e280954a24ae
kidfrom <jason.onggo@tempatkerja.com>
2020-08-31 15:40:06 +0700

    npm install react-bootstrap bootstrap

--- reactjs_statepropsonly/src/pages/Login/index.js
+++ reactjs_statepropsonly/src/pages/Login/index.js
@@ -1,26 +1,8 @@
 import React from "react";
-import { Container, Alert, Form, FormGroup, FormLabel, FormControl } from "../../components";
+import { Container } from "react-bootstrap";
 
 export const Login = () => (
   <div className="min-vh-100 d-flex align-items-center">
-    <Container className="container w-auto text-center">
-      <Form>
-        <FormGroup controlId="username">
-          <FormLabel>Username</FormLabel>
-          <FormControl type="text" placeholder="Enter username" />
-        </FormGroup>
-      </Form>
-      {/* <form>
-        <Alert variant="warning">Error</Alert>
-        <div className="form-group">
-          <label for="username">Username</label>
-          <input type="text" className="form-control form-control-sm" id="username" placeholder="Enter username" />
-        </div>
-        <div className="form-group">
-          <label for="password">Password</label>
-          <input type="password" className="form-control form-control-sm" id="password" placeholder="Enter password" />
-        </div>
-      </form> */}
-    </Container>
+    <Container className="w-auto text-center">Login</Container>
   </div>
 );
