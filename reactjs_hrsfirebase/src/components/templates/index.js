18cef2384db5a4962cf1f6d57bc6c9db524a0778
kidfrom <jason.onggo@tempatkerja.com>
2020-08-31 12:09:08 +0700

    first commit, npx create-react-app

--- reactjs_hrsfirebase/src/components/templates/index.js
+++
@@ -1,1 +0,0 @@
-export { TopNavbarMiddleContent } from "./TopNavbarMiddleContent";
--- reactjs_hrsfirebase/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export { Login } from "./Login";
-export { PasswordReset } from "./PasswordReset";
-export { Dashboard } from "./Dashboard";
-export { AddPage } from "./AddPage";
-export { ListPage } from "./ListPage";
-export { PlacementPage } from "./PlacementPage"
