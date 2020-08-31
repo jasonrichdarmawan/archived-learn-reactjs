42c214d224d353fe88f920bc375ea55da873fa0c
kidfrom <jason.onggo@tempatkerja.com>
2020-08-31 14:07:22 +0700

    add fake fetch for AuthData res

--- reactjs_statepropsonly/src/App.js
+++ reactjs_statepropsonly/src/App.js
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Routes } from "./config";
 import { RenderRoutes } from "./utils";
 
 const App = () => {
   const [AuthData, setAuthData] = useState({ res: "await" });
   const routes = { routes: Routes({ isAuthorized: AuthData.res }) };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAuthData({ ...AuthData, res: false });
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return <RenderRoutes routes={routes} />;
 };
