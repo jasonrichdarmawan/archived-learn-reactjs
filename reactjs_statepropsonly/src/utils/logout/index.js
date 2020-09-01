994f340d137e8ba6f451514013058ba0cab5feaf
kidfrom <jason.onggo@tempatkerja.com>
2020-09-01 19:08:36 +0700

    fix bug handleLogout

--- reactjs_statepropsonly/src/utils/logout/index.js
+++ reactjs_statepropsonly/src/utils/logout/index.js
@@ -1,6 +1,6 @@
 export const handleLogout = ({ authData, setAuthData }) => {
   const timer = setTimeout(() => {
-    setAuthData({ ...authData, res: false, type: undefined });
+    setAuthData({ ...authData, isAuthorized: false, type: undefined });
   }, 1000);
   return () => clearTimeout(timer);
 };
