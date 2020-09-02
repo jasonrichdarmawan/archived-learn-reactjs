9daf8777831ed6c4f6f6676762c6ccdc8d66db6d
kidfrom <jason.onggo@tempatkerja.com>
2020-09-03 05:08:22 +0700

    add listener to changes in Database

--- reactjs_studentdatabasestate/src/providers/AuthData/index.js
+++ reactjs_studentdatabasestate/src/providers/AuthData/index.js
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { DatabaseContext } from "../Database";
 
 // Fake Database
 export const AuthDataContext = React.createContext();
 
 export const AuthDataProvider = ({ children }) => {
   const [authData, setAuthData] = useState({ isAuthorized: "await" });
+  const { database } = useContext(DatabaseContext);
+
+  useEffect(() => {
+    setAuthData({ ...authData, ...database[authData.uid] });
+  }, [database]);
+
   return (
     <AuthDataContext.Provider value={{ authData, setAuthData }}>
       {children}
