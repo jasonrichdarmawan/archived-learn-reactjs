e78bf722d44535825faf4bc8250be38c2bb26870
kidfrom <jason.onggo@tempatkerja.com>
2020-09-03 23:57:37 +0700

    add template for route with subroutes

--- reactjs_hrsredux/src/config/routes/index.js
+++ reactjs_hrsredux/src/config/routes/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 import FetchAuthData from "../../utils/FetchAuthData";
+import RenderRoutes from "../../utils/RenderRoutes";
 
 const routesConfig = ({ isAuthorized }) => {
-  console.log("routesConfig", isAuthorized);
+  // console.log("routesConfig", isAuthorized);
 
   return [
     {
@@ -15,6 +16,38 @@ const routesConfig = ({ isAuthorized }) => {
         else if (isAuthorized === false) return <Redirect to="/login" />;
         else return <FetchAuthData isAuthorized={isAuthorized} />;
       },
+    },
+    {
+      key: "LOGIN",
+      path: "/login",
+      exact: true,
+      component: () => {
+        if (isAuthorized === true) return <Redirect to="/app" />;
+        // TODO LoginPage
+        else if (isAuthorized === false) return "Login";
+        else return <FetchAuthData isAuthorized={isAuthorized} />;
+      },
+    },
+    {
+      key: "APP_ROOT",
+      path: "/app",
+      exact: false,
+      component: (props) => {
+        if (isAuthorized === true) return <RenderRoutes {...props} />;
+        else if (isAuthorized === false) return <Redirect to="/login" />;
+        else return <FetchAuthData isAuthorized={isAuthorized} />;
+      },
+      routes: [
+        {
+          key: "Dashboard",
+          path: "/app",
+          exact: true,
+          // TODO DashboardPage
+          component: () => {
+            return "Dashboard";
+          },
+        },
+      ],
     },
   ];
 };
