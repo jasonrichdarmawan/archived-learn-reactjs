86a54cb0194a12de1d694249ca36b8a0737654c2
kidfrom <jason.onggo@tempatkerja.com>
2020-09-05 04:48:35 +0700

    update motivatonal letter

--- reactjs_cv/src/pages/Motivation/index.js
+++ reactjs_cv/src/pages/Motivation/index.js
@@ -32,6 +32,19 @@ const contentJSX = () => {
         <footer className="blockquote-footer">
           Jack Ma <cite title="Source Title">, Alibaba Group</cite>
         </footer>
+        <br />
+        <p className="mb-0">
+          Apple suffered for several years from lousy engineering management. I
+          have to say it. There were people that were going off in 18 different
+          directions doing arguably interesting things in each one of them. Good
+          engineers, lousy management. ... So we had to decide, what are the
+          fundamental directions we are going in, what make sense and what
+          doesn't ... And the results to that focus is gonna be some really
+          great products ...
+        </p>
+        <footer className="blockquote-footer">
+          Steve Jobs <cite title="Source Title">, Apple</cite>
+        </footer>
       </blockquote>
     </Container>
   );
