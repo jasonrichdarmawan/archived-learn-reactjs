cb7fa99ab0c970d658b7d6a21f1e8736a3d83488
Jason <jason.onggo@tempatkerja.com>
2020-09-22 10:19:07 +0700

    add basic list sales orders

--- reactjs_erd/src/app/store.js
+++ reactjs_erd/src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import authStateReducer from "features/AuthState/AuthStateSlice";
 import employeeReducer from "features/Employee/EmployeeSlice";
+import salesOrdersReducer from "features/Sales/SalesOrdersSlice";
 
 export default configureStore({
   reducer: {
     authState: authStateReducer,
     employee: employeeReducer,
+    salesOrders: salesOrdersReducer,
   },
 });
