8c50dc9ef8620e61bd0d83aa5f53b9ec4b3b1ad9
Jason <jason.onggo@tempatkerja.com>
2020-10-02 16:36:15 +0700

    first commit

--- reactjs_erd/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { Loading } from "./Loading";
-export { AlertFormButton } from "./AlertFormButton";
-export { TopNavbar } from "./TopNavbar";
-export { ResponsiveTable } from "./ResponsiveTable";
--- reactjs_erd/src/features/index.js
+++
@@ -1,6 +0,0 @@
-export { Dashboard } from "./Dashboard";
-export { Login } from "./Login";
-export { Employee } from "./Employee";
-export { Sales } from "./Sales";
-export { Inventory } from "./Inventory";
-export { Purchase } from "./Purchase";
